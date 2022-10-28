29b7db3dcd7604e6b4870922f4c20499b05e5c3d
Bever1337 <bever1337@posteo.net>
2022-10-27 21:09:12 -0400

    adds router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Provider } from "react-redux";
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 
 import { Authenticator } from "./components/authenticator";
-// import { RarityTab } from "./components/rarity-tab";
+import { MapsContainer } from "./components/maps-container";
 import { MaterialsContainer } from "./components/materials-container";
 import { pouch } from "./features/pouch";
 import { store } from "./store";
@@ -35,27 +36,37 @@ const appStore = store();
 // ];
 
 export function App() {
-  const [ready, setReady] = useState(false);
-  useEffect(() => {
-    // pouch
-    //   .load(process.env.PUBLIC_URL + "/dump.txt")
-    //   .then(() =>
-    pouch
-      .createIndex({
-        index: { fields: ["$id", "rarity", "type"] },
-      })
-      // )
-      .then(() => {
-        setReady(true);
-      })
-      .catch((err) => {
-        console.warn("went wrong", err);
-      });
-  }, []);
+  const [ready, setReady] = useState(true);
+  // useEffect(() => {
+  //   // pouch
+  //   //   .load(process.env.PUBLIC_URL + "/dump.txt")
+  //   //   .then(() =>
+  //   pouch
+  //     .createIndex({
+  //       index: { fields: ["$id", "rarity", "type"] },
+  //     })
+  //     // )
+  //     .then(() => {
+  //       setReady(true);
+  //     })
+  //     .catch((err) => {
+  //       console.warn("went wrong", err);
+  //     });
+  // }, []);
   return (
     <Provider store={appStore}>
-      <Authenticator />
-      {ready ? <MaterialsContainer /> : null}
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <nav>
+          <Authenticator />
+          <Link to="/maps">maps</Link>
+          <Link to="/materials-storage">storage</Link>
+        </nav>
+        <hr />
+        <Routes>
+          <Route element={<MapsContainer />} path="/maps" />
+          <Route element={<MaterialsContainer />} path="/materials-storage" />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
