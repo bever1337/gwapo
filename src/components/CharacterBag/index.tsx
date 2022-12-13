7f33275fbcb7d79a74cbae2a1ceb4fec1c1a40b3
Bever1337 <bever1337@posteo.net>
2022-12-12 23:01:45 -0500

    feat(vault): Queries for character bags

--- src/components/CharacterBag/index.tsx
+++ src/components/CharacterBag/index.tsx
@@ -1,3 +1,4 @@
+import { skipToken } from "@reduxjs/toolkit/dist/query";
 import { useState } from "react";
 
 import { AccordionControl } from "../Accordion/Control";
@@ -10,22 +11,30 @@ import { classNames } from "../../features/css/classnames";
 import { readItems } from "../../features/store/api/read-items";
 import type { Bag } from "../../features/store/api/read-characters-inventory";
 
-export function CharacterBag(props: { characterBag: Bag; bagIndex: number }) {
+export function CharacterBag(props: {
+  characterBag: Bag | null;
+  bagIndex: number;
+}) {
   const [open, setOpen] = useState(true);
-  const { data: items } = readItems.useQuery({
-    ids: props.characterBag.inventory.reduce(
-      (acc, item) => (item ? acc.concat([item.id]) : acc),
-      [] as number[]
-    ),
-  });
+  const skip = props.characterBag === null;
+  const queryArguments = {
+    ids:
+      props.characterBag?.inventory.reduce(
+        (acc, item) => (item ? acc.concat([item.id]) : acc),
+        [props.characterBag.id] as number[]
+      ) ?? [],
+  };
+  const { data: items } = readItems.useQuery(
+    skip ? skipToken : queryArguments,
+    { skip }
+  );
   return (
     <section
       className={classNames(materialsClasses["materials__inline-wrapper"])}
     >
       <div className={classNames(accordionClasses["tab"])}>
         <h2 className={classNames(accordionClasses["tab__heading"])}>
-          {/* todo, query DB for item name */}
-          {""}
+          {items?.entities[props.characterBag?.id ?? ""]?.name ?? ""}
         </h2>
         <AccordionControl onChange={setOpen} open={open} />
       </div>
@@ -35,7 +44,7 @@ export function CharacterBag(props: { characterBag: Bag; bagIndex: number }) {
           !open && hideClasses["hide"]
         )}
       >
-        {props.characterBag.inventory.map((characterBagItem, index) => (
+        {props.characterBag?.inventory.map((characterBagItem, index) => (
           // Warning: bags do not have any unique identifiers
           // Features like filtering and sorting will not work until each item has a uid
           // For now, we can assume this is safe because the list is static
