36704aab6a7f612eaab2205debb561a503427904
Bever1337 <bever1337@posteo.net>
2022-12-14 00:37:41 -0500

    feat(vault): Adds item rarity borders

--- src/components/vault-item.tsx
+++ src/components/vault-item.tsx
@@ -1,5 +1,7 @@
+import itemTileClasses from "./ItemTile/index.module.css";
 import materialsClasses from "./material.module.css";
 
+import { classNames } from "../features/css/classnames";
 import type { AccountBankItem } from "../features/store/api/read-account-bank";
 import type { Item } from "../types/item";
 
@@ -8,9 +10,13 @@ export function VaultItem(props: {
   item?: Item;
 }) {
   return (
-    <li className={[materialsClasses["material__item"]].join(" ")}>
+    <li className={classNames(materialsClasses["material__item"])}>
       <img
-        className={[materialsClasses["material__img"]].join(" ")}
+        className={classNames(
+          materialsClasses["material__img"],
+          itemTileClasses["tile"],
+          itemTileClasses[props.item?.rarity ?? ""]
+        )}
         alt={props.item?.name ?? ""}
         src={props.item?.icon}
       />
